39deb7a90192967d22d697f7c65bfa88aead81f0
Anoosha <anoosha.sanugula@everest.engineering>
2024-07-04 11:09:26 +0530

    reading data function in authors.crud.ts

--- crud/authors.crud.ts
+++ crud/authors.crud.ts
@@ -1,10 +1,20 @@
 import { authors } from "../Models/authors";
 
 //function to create a new data
-function createData(data:any){
-    const author = authors.create(data);
-    console.log("created user");
-}
+// function createData(data:any){
+//     const author = authors.create(data);
+//     console.log("created author");
+// }
+async function createData(data:any) {
+    try {
+        const author = await authors.create(data);
+        console.log('created author');
+        
+      } catch (error) {
+      console.error('Error in creating author');
+      throw error;
+    }
+  }
 
 const data={
     name: 'Hareesh', 
@@ -12,3 +22,26 @@ const data={
     nationality:'India'
 };
 createData(data);
+
+
+async function readData(id:any) {
+    try {
+      const author = await authors.findOne({ where: { id: id } });
+      if (author) {
+        console.log('Author found:', author.toJSON());
+        return author;
+      } else {
+        console.log('Author not found');
+        return null;
+      }
+    } catch (error) {
+      console.error('Error finding author:', error);
+      throw error;
+    }
+  }
+  
+ 
+const authorId = 3; 
+readData(authorId);
+
+
